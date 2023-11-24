d5ac87807ed92992c8a472f01c0bf27e81418b8c
halammi3 <halammi3@fel.cvut.cz>
2023-11-24 15:42:54 +0100

    Prvni test detailu detail_u detail_aktivity

--- js/actions.js
+++ js/actions.js
@@ -1,3 +1,55 @@
+// Inicializace Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBREpNaltCczA4mw8MXPFvodWsmNzeI5sw",
+  authDomain: "pwa-camp-calendar.firebaseapp.com",
+  projectId: "pwa-camp-calendar",
+  storageBucket: "pwa-camp-calendar.appspot.com",
+  messagingSenderId: "371363956824",
+  appId: "1:371363956824:web:937bb8bebc462536e8622d",
+  measurementId: "G-BMEXF6MDF0"
+};
+const app = firebase.initializeApp(firebaseConfig);
+
+// Získání instance Firestore
+const db = app.firestore(app);
+let currentDay = null;
+
+function saveId(id) {
+        
+  db.collection("actions")
+  .where("id", "==", id )
+  .limit(1)
+  .get()
+  .then(querySnapshot => {
+      console.log( querySnapshot.docs[0].data() );
+      renderDay( querySnapshot.docs[0].data() );
+      currentDay = querySnapshot.docs[0].data();
+  })
+  .catch((error) => {
+      console.log("Error getting documents: ", error);
+      renderEmptyDay();
+  });
+}
+
+function getDayData(id) {
+        
+  db.collection("actions")
+  .where("id", "==", id )
+  .limit(1)
+  .get()
+  .then(querySnapshot => {
+      console.log( querySnapshot.docs[0].data() );
+      React.useEffect(() => {
+        actionsElement.innerHTML = OdpolednInnost();
+    }, []);
+      return querySnapshot.docs[0].data();
+  })
+  .catch((error) => {
+      console.log("Error getting documents: ", error);
+  });
+}
+
+
 function timestampToHHMM(timestamp, addMM) {
   // Převedení Timestamp na objekt Date
   const date = new Date((timestamp.seconds * 1000 + timestamp.nanoseconds / 1e6) + addMM * 60 * 1000);
@@ -10,6 +62,13 @@ function timestampToHHMM(timestamp, addMM) {
   const formattedTime = `${hours}:${minutes}`;
 
   return formattedTime;
+}
+
+function goToDetail(activity_id, day_id){
+  console.log(currentDay)
+     // window.location = 'activity_detail.html?activity=' + activity_id + '&day='+ day_id;
+     renderActivity(currentDay);
+  return;
 }
 
 // Vypíše rozvrh na celý den, parametr day obsahuje všechny informace, na kterých rozvrh závisí
@@ -27,7 +86,7 @@ function renderDay(day) {
               <div class="action-details">
                 <div class="action-title">Budíček</div>
               </div>
-              <div class="action-edit">
+              <div onclick="goToDetail(1 , ${day.id})" class="action-edit">
                 <i class="material-icons">chevron_right</i>
               </div>
             </div>
@@ -36,7 +95,7 @@ function renderDay(day) {
               
             </div>
 
-            <div class="card-panel action white row">
+            <div  class="card-panel action white row">
               <div class="action-time-normal">
                 <div class="action-time-start">${timestampToHHMM(day.startOfDay, 30)}</div>
               </div>
@@ -53,7 +112,7 @@ function renderDay(day) {
               <div class="action-details">
                 <div class="action-title">${day.dopoAction}</div>
               </div>
-              <div class="action-edit">
+              <div onclick="goToDetail(2)" class="action-edit">
                 <i class="material-icons">chevron_right</i>
               </div>
             </div>
@@ -80,7 +139,7 @@ function renderDay(day) {
                 <div class="action-details">
                   <div class="action-title">${day.odpoAction}</div>
                 </div>
-                <div class="action-edit">
+                <div onclick="goToDetail(3)" class="action-edit">
                   <i class="material-icons">chevron_right</i>
                 </div>
               </div>
@@ -101,7 +160,7 @@ function renderDay(day) {
                 <div class="action-details">
                   <div class="action-title">${day.podAction}</div>
                 </div>
-                <div class="action-edit">
+                <div onclick="goToDetail(4)" class="action-edit">
                   <i class="material-icons">chevron_right</i>
                 </div>
               </div>
@@ -142,7 +201,7 @@ function renderDay(day) {
                 <div class="action-details">
                   <div class="action-title">Večerka</div>
                 </div>
-                <div class="action-edit">
+                <div onclick="goToDetail(5)" class="action-edit">
                   <i class="material-icons">chevron_right</i>
                 </div>
               </div>
@@ -164,7 +223,7 @@ function renderDay(day) {
             <div class="action-details">
                   <div class="action-title">Rozcvička</div>
             </div>
-            <div class="action-edit">
+            <div onclick="goToDetail(0)" class="action-edit">
               <i class="material-icons">chevron_right</i>
             </div>
           </div>
@@ -179,7 +238,7 @@ function renderDay(day) {
             <div class="action-details">
               <div class="action-title">Nástup</div>
             </div>
-            <div class="action-edit">
+            <div onclick="goToDetail(6)" class="action-edit">
               <i class="material-icons">chevron_right</i>
             </div>
           </div>
@@ -313,5 +372,48 @@ function renderEmptyDay() {
           <div class="right">
             <a class="btn-floating btn-large add-btn sidenav-trigger" data-target="create-form">Vytvořit rozvrh</a>
           </div>
+        `
+}
+
+function renderActivity(day) {
+
+  const actionsElement = document.getElementById('day-actions');
+
+  actionsElement.innerHTML = `
+  <body>
+  <div class="odpoledn-innost">
+    <div class="div">
+      <div class="overlap">
+        <div class="rectangle">
+
+          <div class="as-bud-ku-mus-b-t">Čas od: ${timestampToHHMM(day.startOfDay, 15)}</div>
+          <div class="text-wrapper">${day.odpoAction}</div>
+          <div class="overlap-group">
+            <div class="content-wrapper">
+              <div class="content">
+                <div class="div-wrapper"><div class="label-text-2">Název činnosti</div></div>
+                <div class="placeholder-text"><div class="placeholder-text-2">Kamenný Labyrint</div></div>
+              </div>
+            </div>
+            <div class="state-layer-2">
+              <div class="content">
+                <div class="label-text-3"><div class="label-text-2">Popis činnosti</div></div>
+                <div class="placeholder-text-wrapper">
+                  <p class="p">
+                    Oddíly mají za úkol postavit v prvním <br />lese z kamenů labyrint vhodné <br />velikosti k tomu,
+                    aby jej děti probíhaly.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="text-wrapper-2">Čas do: ${timestampToHHMM(day.startOfDay, 15)}</div>
+          <div class="button-dal">
+        <div class="label-text-wrapper"><div class="label-text">Upravit</div></div>
+      </div>
+      </div>
+    </div>
+  </div>
         `
 }
---
+++ js/test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { AppbarTvorba } from "./AppbarTvorba";
+import { ArrowBack24Px } from "./ArrowBack24Px";
+import { Button } from "./Button";
+import "./style.css";
+
+export const OdpolednInnost = () => {
+  return (
+   
+  );
+};
