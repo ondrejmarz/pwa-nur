3aaa98605fedcf6df4f6cba2b5631ff1a187b48b
Hana Litavska <hana.litavska@gmail.com>
2023-11-25 13:41:51 +0100

    Fix repeated create form opening

--- js/actions.js
+++ js/actions.js
@@ -514,5 +514,49 @@ function renderCreateForm() {
  </div>
   `;
 
-  actionsElement.appendChild(script)
+  console.log("script loaded")
+const multiStepForm = document.querySelector("[data-multi-step]")
+const formSteps = [...multiStepForm.querySelectorAll("[data-step]")]
+let currentStep = formSteps.findIndex(step => {
+  return step.classList.contains("active")
+})
+
+if (currentStep < 0) {
+  currentStep = 0
+  showCurrentStep()
+}
+
+multiStepForm.addEventListener("click", e => {
+  console.log("script loaded 1")
+  let incrementor
+  if (e.target.matches("[data-next]")) {
+    incrementor = 1
+  } else if (e.target.matches("[data-prev]")) {
+    incrementor = -1
+  }
+
+  if (incrementor == null) return
+
+  const inputs = [...formSteps[currentStep].querySelectorAll("input")]
+  const allValid = inputs.every(input => input.reportValidity())
+  if (allValid) {
+    currentStep += incrementor
+    showCurrentStep()
+    console.log("show current step")
+  }
+})
+/*
+formSteps.forEach(step => {
+  step.addEventListener("animationend", e => {
+    formSteps[currentStep].classList.remove("hide")
+    e.target.classList.toggle("hide", !e.target.classList.contains("active"))
+  })
+}) */
+
+function showCurrentStep() {
+  console.log("showing current step")
+  formSteps.forEach((step, index) => {
+    step.classList.toggle("active", index === currentStep)
+  })
+}
 }
